b4dd766d21c86dec141e3d4ba17b77e9b19c6eef
Marco <gsoccerstar007@gmail.com>
2021-10-11 20:51:40 -0400

    seekbar added and timer set up

---
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        TextView timerTextView = findViewById(R.id.textView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int minutes = i / 60;
+                int seconds = i - (minutes * 60);
+
+                String secondString = Integer.toString(seconds);
+
+                if (secondString.equals("0")) {
+                    secondString = "00";
+
+                }
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}

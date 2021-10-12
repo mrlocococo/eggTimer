6897f5615d05eb0850140270cdf74054bed0ca00
Marco <gsoccerstar007@gmail.com>
2021-10-11 22:03:38 -0400

    added the ability to stop the timer and disable the seekbar while timer is active

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,23 +15,44 @@ public class MainActivity extends AppCompatActivity {
 
     TextView timerTextView;
     SeekBar timerSeekBar;
+    Boolean counterIsActive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
 
     public void timer(View view) {
-        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
 
-            @Override
-            public void onTick(long l) {
-                updateTimer((int) l / 1000);
+        if (counterIsActive) {
 
-            }
-            //plays sound when timer is finished
-            @Override
-            public void onFinish() {
-                MediaPlayer mpplayer = MediaPlayer.create(getApplicationContext(),R.raw.zhu);
-                mpplayer.start();
+            timerTextView.setText("0:30");
+            timerSeekBar.setProgress(30);
+            timerSeekBar.setEnabled(true);
+            countDownTimer.cancel();
+            goButton.setText("Start!");
+            counterIsActive = false;
 
-            }
-        }.start();
+        } else {
+
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("STOP!");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long l) {
+                    updateTimer((int) l / 1000);
+
+                }
+
+                //plays sound when timer is finished
+                @Override
+                public void onFinish() {
+                    MediaPlayer mpplayer = MediaPlayer.create(getApplicationContext(), R.raw.zhu);
+                    mpplayer.start();
+
+                }
+            }.start();
+        }
     }
 
     public void updateTimer(int secondsLeft) {
@@ -54,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
         timerSeekBar = findViewById(R.id.timerSeekBar);
         timerTextView = findViewById(R.id.textView);
+        goButton = findViewById(R.id.eggButton);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
